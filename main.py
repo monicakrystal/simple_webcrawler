cefd6874b3a369a84b4bf2f78b019093592d679c
monicakrystal <monicakrystal@icloud.com>
2024-03-27 18:47:41 -0700

    created webcrawl function

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+
+def start_webcrawl():
+    url = ''
+    html = urllib.request.urlopen(url).read()
+    soups = BeautifulSoup(html, 'html.parser')
+    for soup in soups:
+        print(soup)
 
 
 
 if __name__ == '__main__':
-
+    start_webcrawl()
 
