c3ed8e1225bc2665853f55d8ca13c71d0ad29243
monicakrystal <monicakrystal@icloud.com>
2024-03-28 08:19:01 -0700

    possibly completed webcrawl

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
 
-def start_webcrawl():
-    url = ''
-    html = urllib.request.urlopen(url).read()
-    soups = BeautifulSoup(html, 'html.parser')
-    for soup in soups:
-        print(soup)
+def start_webcrawl(link):
+    string_soup = ""
+    with open(link) as file_of_websites:
+        for website in file_of_websites:
+            url = website.strip()
+            html = urllib.request.urlopen(url).read()
+            soup = BeautifulSoup(html, 'html.parser')
+            string_soup += str(soup)
+    return string_soup
 
-
+def write_names(lines, file_name_to_write_to):
+    with open(file_name_to_write_to, "w") as f:
+        f.write(lines)
 
 if __name__ == '__main__':
-    start_webcrawl()
+    crawl_results = start_webcrawl('five_urls.txt')
+    write_names(crawl_results, 'copied_html.txt')
 
